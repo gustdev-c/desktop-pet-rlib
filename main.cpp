fcce6b134841ee05ca1c4ff5737f27c803eae28d
Gust <gustdevo@gmail.com>
2026-09-21 22:45:24 -0300

    Added the actual pet (transparent, undecorated topmost window) and basic movement

--- main.cpp
+++ main.cpp
@@ -1,15 +1,30 @@
 #include "raylib.h"
+#include <X11/Xlib.h>
+
+typedef struct Pet {
+	Texture sprite;
+} Pet;
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib example - basic window");
+	SetConfigFlags(FLAG_WINDOW_TRANSPARENT | FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_TOPMOST);
+    InitWindow(64, 64, nullptr);
+	Pet petSlime = (Pet){
+		.sprite = LoadTexture("slimetemp.png"),
+	};
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(BLANK);
+			DrawTexture(petSlime.sprite, 0, 0, RAYWHITE);
         EndDrawing();
+
+		SetWindowPosition(GetWindowPosition().x, GetWindowPosition().y + 1000 * GetFrameTime());
+
+		if(IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+			SetWindowPosition(GetMouseX() * (GetMonitorWidth(GetCurrentMonitor()) + GetWindowPosition().x), GetMouseY() * (GetMonitorHeight(GetCurrentMonitor()) + GetWindowPosition().y));
+		}
     }
 
     CloseWindow();
